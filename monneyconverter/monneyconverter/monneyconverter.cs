2ee15937ef36b0f2d7b5e6022741c1b176b08706
sarduk <kidici2@gmail.com>
2018-06-11 23:43:59 +0200

    2343lastchanges
    descr 2343lastchanges

--- monneyconverter/monneyconverter/monneyconverter.cs
+++ monneyconverter/monneyconverter/monneyconverter.cs
@@ -23,7 +23,52 @@ namespace monneyconverter
         {
             /*return method*/
 
-            return "MCCLV";
+            IDictionary<int, string> roman = new Dictionary<int, string>();
+
+            roman[1000] = "M";
+            roman[900] = "CM";
+            roman[500] = "D";
+            roman[400] = "CD";
+            roman[100] = "C";
+            roman[90] = "XC";
+            roman[50] = "L";
+            roman[40] = "XL";
+            roman[10] = "X";
+            roman[9] = "IX";
+            roman[5] = "V";
+            roman[4] = "IV";
+            roman[1] = "I";
+
+
+
+            //https://stackoverflow.com/questions/141088/what-is-the-best-way-to-iterate-over-a-dictionary
+            /*foreach (IDictionary<int, string> entry in roman)
+            {
+                // do something with entry.Value or entry.Key
+            }*/
+
+            foreach (KeyValuePair<int, string> entry in roman)
+            {
+
+            }
+            
+
+                /*
+                 * 
+                          def roman_num(num):
+                                for r in roman.keys():
+                                    x, y = divmod(num, r)
+                                    yield roman[r] * x
+                                    num -= (r* x)
+                                    if num > 0:
+                                        roman_num(num)
+                                    else:
+                                        break
+
+                            return "".join([a for a in roman_num(num)])
+                */
+
+                return "MCCLV";
 
         }
 
